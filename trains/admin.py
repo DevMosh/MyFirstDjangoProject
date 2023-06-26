ad1b8c79228f125750b2978bbd7a14d6f2436c4f
musicvsembot <49912219+DevMosh@users.noreply.github.com>
2023-06-27 02:16:31 +0300

    improve display admin panel Trains

--- trains/admin.py
+++ trains/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from trains.models import Train
 
-admin.site.register(Train)
+
+# делаем данные в админке более информативными
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+
+    # даст возможность редактировать поле с главной страницы
+    list_editable = ('travel_time', )  # 'from_city' - изменение таких данных увеличит нагрузку
+
+
+admin.site.register(Train, TrainAdmin)
